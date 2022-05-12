b2ded3f19d0323effd974f3a190683f2964ba912
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-11 19:14:25 -0700

    add productag and tag categories

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,29 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
-
 class ProductTag extends Model {}
-
 ProductTag.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:false,
+      autoIncrement:false,
+    },
+    product_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'Product',
+        key:'id',
+      },
+    },
+    tag_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'tag',
+        key:'id',
+      },
+    },
   },
   {
     sequelize,
@@ -16,5 +33,4 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
-
 module.exports = ProductTag;
