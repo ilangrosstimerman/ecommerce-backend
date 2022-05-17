f86b8f533459eb814eedd75fe9b79de9359ba061
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-16 18:21:00 -0700

    add tag routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,24 +1,61 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
-
 router.get('/', async (req, res) => {
+
   try{
     const categoryData = await Category.findAll({
       include:[{model:Product}],
     });
     res.status(200).json(categoryData);
-
   } catch(err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id,
+      {include: [Product]}
+    );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-router.post('/', (req, res) => {
+
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-router.put('/:id', (req, res) => {
+
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 module.exports = router;
